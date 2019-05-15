bc14299d0d3745126688b4dda1729d5a42a24b0c
aligebaba <372229919@qq.com>
2019-05-15 22:00:10 +0800

    更改

--- C++_primer_plus/C02/5_ourfunc.cpp
+++ C++_primer_plus/C02/5_ourfunc.cpp
@@ -1,11 +1,12 @@
 // ourfunc.cpp -- defining your own function
 
 #include <iostream>
-using namespace std;
+
 void simon(int);
 
 int main()
-{    
+{
+    using namespace std; 
     simon(3);
     cout << "Pick an integer: ";
     int count;
@@ -20,5 +21,6 @@ int main()
 
 void simon(int n)
 {
+    using namespace std;
     cout << "Simon says touch your toes " << n << " times." << endl;
 }
