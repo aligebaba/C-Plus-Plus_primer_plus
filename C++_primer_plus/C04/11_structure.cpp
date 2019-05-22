4ba50fdd55fe8a750450324123784a8897711d20
aligebaba <372229919@qq.com>
2019-05-22 21:56:30 +0800

    更改

---
+++ C++_primer_plus/C04/11_structure.cpp
@@ -0,0 +1,26 @@
+// structure.cpp -- a simple structure
+
+#include <iostream>
+
+struct inflatable
+{
+    char name[20];
+    float volume;
+    double price;
+};
+
+int main()
+{
+    using namespace std;
+    inflatable guest = {"Glorious Gloria", 1.88, 29.99};    // “=”可以省略。
+    inflatable pal = {"Audacious Arthur", 3.12, 32.99};
+
+    cout << "Expand your guest list with " << guest.name;
+    cout << " and " << pal.name << "!\n";
+
+    cout << "You can have both for $";
+    cout << guest.price + pal.price << "!\n";
+
+    cin.get();
+    return 0;
+}
