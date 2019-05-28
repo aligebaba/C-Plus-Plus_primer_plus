2180bda29a6958992404fdd1b4ae33e99ea86b4c
aligebaba <372229919@qq.com>
2019-05-28 21:31:44 +0800

    更改

---
+++ C++_primer_plus/C06/14_cingolf.cpp
@@ -0,0 +1,34 @@
+// cingolf.cpp -- non-numeric input skipped
+
+#include <iostream>
+
+const int Max = 5;
+
+int main()
+{
+    using namespace std;
+    int golf[Max];
+    cout << "Please enter your golf scores.\n";
+    cout << "You must enter " << Max << " rounds.\n";
+    int i;
+    for (i = 0; i < Max; i++)
+    {
+        cout << "round #" << i + 1 << ": ";
+        while (!(cin >> golf[i]))
+        {
+            cin.clear();                // 重置输入。
+            while (cin.get() != '\n')   // 删除错误输入。
+                continue;
+            cout << "Please enter a number: ";
+        }
+    }
+    double total = 0.0;
+    for (i = 0; i < Max; i++)
+        total += golf[i];
+    cout << total / Max << " = average score "
+         << Max << " rounds\n";
+
+    cin.get();
+    cin.get();
+    return 0;
+}
