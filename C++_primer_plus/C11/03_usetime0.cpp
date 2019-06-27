cea374ec7426e60d0bffe292bfa6e5c55206b38a
aligebaba <372229919@qq.com>
2019-06-27 20:26:26 +0800

    更改

---
+++ C++_primer_plus/C11/03_usetime0.cpp
@@ -0,0 +1,34 @@
+// usetime0.cpp -- using the first draft of the Time class
+
+#include <iostream>
+#include "01_mytime0.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    cout << "planning time = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "coding time = ";
+    coding.Show();
+    cout << endl;
+
+    cout << "fixing time = ";
+    fixing.Show();
+    cout << endl;
+
+    total = coding.Sum(fixing);
+    cout << "coding.Sum(fixing) = ";
+    total.Show();
+    cout << endl;
+
+    std::cin.get();
+    return 0;
+}
